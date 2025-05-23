2007a0b2a788f2ef79ef13c4448b93c26f3c88d4
Axel Biernastki Otto <axel.otto@bs.nttdata.com>
2025-05-23 19:02:20 +0000

    Adicionado os botões para ver cadastro de clientes e produtos

--- app/cadastroproduto/annotations.cds
+++ app/cadastroproduto/annotations.cds
@@ -1,1 +1,12 @@
 using ShopService as service from '../../srv/services';
+annotate service.Produtos with @(
+    UI.HeaderFacets : [
+        
+    ],
+    UI.FieldGroup #Administrao : {
+        $Type : 'UI.FieldGroupType',
+        Data : [
+        ],
+    }
+);
+
--- app/cadastroproduto/webapp/ext/controller/Login.js
+++ app/cadastroproduto/webapp/ext/controller/Login.js
@@ -9,6 +9,12 @@ sap.ui.define([
         },
         voltarInicio: function() {
             window.location.href = '/lojacap/webapp/index.html'
+        },
+        irClientesCadastrados: function() {
+            window.location.href = '/clientes/webapp/index.html'
+        },
+        verProdutosCadastrados: function() {
+            window.location.href = '/produtos/webapp/index.html'
         }
     };
 });

e8dc8d85eae4d89028a9cb321276170026900383
Thiago Vinicius Oliveira de Souza <thiago.desouza@bs.nttdata.com>
2025-06-23 13:15:06 +0000

    mudanças de melhores praticas no botão de ver logs ao inves de filter + refresh agora ele faz um filtro odata. Formatação de codigo de tudo sobre o monitor

--- srv/lojacap/cliente.js
+++ srv/lojacap/cliente.js
@@ -4,34 +4,34 @@ const cds = require('@sap/cds');
 module.exports = function (srv) {
   const { Clientes } = srv.entities;
 
-    // 🔐 loginCliente
-    srv.on('loginCliente', async req => {
-      const { email, senha } = req.data;
-      const user = await SELECT.one.from(Clientes).where({ email, senha });
-      if (!user) return req.reject(401, 'Login inválido');
+  // 🔐 loginCliente
+  srv.on('loginCliente', async req => {
+    const { email, senha } = req.data;
+    const user = await SELECT.one.from(Clientes).where({ email, senha });
+    if (!user) return req.reject(401, 'Login inválido');
 
-      await UPDATE(Clientes).set({}).where({ ID: user.ID });
-      await UPDATE('my.shop.Cliente')
-        .set({ createdBy: req.user.id })
-        .where({ ID: user.ID });
+    await UPDATE(Clientes).set({}).where({ ID: user.ID });
+    await UPDATE('my.shop.Cliente')
+      .set({ createdBy: req.user.id })
+      .where({ ID: user.ID });
 
-      return 'OK';
-    });
+    return 'OK';
+  });
 
-    // 🛂 before CREATE/UPDATE Clientes
-    srv.before(['CREATE', 'UPDATE'], 'Clientes', async req => {
-      const { nome, email, senha } = req.data;
-      if (!nome || !email || !senha)
-        return req.error(400, 'Todos os campos são obrigatórios');
+  // 🛂 before CREATE/UPDATE Clientes
+  srv.before(['CREATE', 'UPDATE'], 'Clientes', async req => {
+    const { nome, email, senha } = req.data;
+    if (!nome || !email || !senha)
+      return req.error(400, 'Todos os campos são obrigatórios');
 
-      if (nome.length < 6)
-        return req.error(400, 'O nome deve ter pelo menos 6 caracteres.');
+    if (nome.length < 6)
+      return req.error(400, 'O nome deve ter pelo menos 6 caracteres.');
 
-      if (senha.length < 6)
-        return req.error(400, 'A senha deve ter pelo menos 6 caracteres.');
+    if (senha.length < 6)
+      return req.error(400, 'A senha deve ter pelo menos 6 caracteres.');
 
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(email))
-        return req.error(400, 'Formato de e-mail inválido.');
-    });
-  }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email))
+      return req.error(400, 'Formato de e-mail inválido.');
+  });
+}
